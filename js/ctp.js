0d6a237123ed19ec97f7ffa15f8efca255394bda
Chris Tracey <ctracey@192-168-1-2.tpgi.com.au>
2012-05-14 22:28:19 +1000

    ctp initial commit

---
+++ js/ctp.js
@@ -0,0 +1,52 @@
+
+function initiateSideMenu() {
+	$("#sidePanelHeader").height("20%");
+	$("#infoIcon").click(function() { showInfo(); });
+	//$("#galleryIcon").click(function() { showGalleryMenu(); });
+	$("#ordersAndPrintsIcon").click(function() { showOrdersAndPrinting(); });
+	$("#contactsIcon").click(function() { showContacts(); });
+	$("#narrower").click(function() { showSideMenu(); });
+}
+
+function showInfo() {
+	sidePanelWiden();
+	$('#sidePanelContent').load('info.html');
+}
+
+function showGalleryMenu() {
+	sidePanelWiden();
+	$('#sidePanelContent').load('galleryMenu.html');
+}
+
+function showOrdersAndPrinting() {
+	sidePanelWiden();
+	$('#sidePanelContent').load('orderprints.html');
+}
+
+function showContacts() {
+	sidePanelWiden();
+	$('#sidePanelContent').load('contacts.html');
+}
+
+function showSideMenu() {
+	sidePanelNarrow();
+	$('#sidePanelContent').load('sideMenu.html', function() {initiateSideMenu();});
+}
+
+function sidePanelWiden() {
+	$("#ctpSidePanel").removeClass("ctpSidePanelNarrow");
+	$("#ctpSidePanelBackground").removeClass("ctpSidePanelNarrow");
+	$("#ctpSidePanel").addClass("ctpSidePanelWide");				
+	$("#ctpSidePanelBackground").addClass("ctpSidePanelWide");
+	$("#ctpIcons").hide('slow');
+	$("#narrower").show('fast');
+}
+
+function sidePanelNarrow() {
+	$("#ctpSidePanel").removeClass("ctpSidePanelWide");
+	$("#ctpSidePanelBackground").removeClass("ctpSidePanelWide");
+	$("#ctpSidePanel").addClass("ctpSidePanelNarrow");
+	$("#ctpSidePanelBackground").addClass("ctpSidePanelNarrow");				
+	$("#narrower").hide('fast');
+	$("#ctpIcons").show('slow');
+}
