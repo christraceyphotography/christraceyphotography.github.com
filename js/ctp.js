3b694976ab62e832b5c212ce4f334fbd7b70744e
Chris Tracey <tracey.chris@gmail.com>
2014-04-27 19:03:23 +1000

    cleaning up unused code

--- js/ctp.js
+++ js/ctp.js
@@ -10,11 +10,6 @@ function show_500px_photo(event){
   open_tab(photo_url);
 }
 
-function init_500px_links() {
-  create_500px_link('.500px_gallery');
-  create_500px_link('.500px_contact');
-}
-
 function handler(event) {
   target = event.target;
   target_class = target.className
@@ -23,65 +18,6 @@ function handler(event) {
   open_tab(url);
 }
 
-function create_500px_link(link_class) {
-  url = $(link_class).attr('url')
-  $(link_class).click(handler)
-}
-
 function open_tab(url) {
   window.open(url);
-}
-
-function initiateSideMenu() {
-	$("#sidePanelHeader").height("20%");
-	$("#infoIcon").click(function() { showInfo(); });
-	$("#ordersAndPrintsIcon").click(function() { showOrdersAndPrinting(); });
-	$("#contactsIcon").click(function() { showContacts(); });
-	$("#narrower").click(function() { showSideMenu(); });
-  init_500px_links();
-}
-
-function showInfo() {
-	sidePanelWiden();
-	$('#sidePanelContent').load('info.html');
-}
-
-function showGalleryMenu() {
-	sidePanelWiden();
-	$('#sidePanelContent').load('galleryMenu.html');
-}
-
-function showOrdersAndPrinting() {
-	sidePanelWiden();
-	$('#sidePanelContent').load('orderprints.html', function() { init_500px_links(); });
-}
-
-function showContacts() {
-	sidePanelWiden();
-	$('#sidePanelContent').load('contacts.html', function() { init_500px_links(); });
-}
-
-function showSideMenu() {
-	sidePanelNarrow();
-	$('#sidePanelContent').load('sideMenu.html', function() {
-      initiateSideMenu();
-  });
-}
-
-function sidePanelWiden() {
-	$("#ctpSidePanel").removeClass("ctpSidePanelNarrow");
-	$("#ctpSidePanelBackground").removeClass("ctpSidePanelNarrow");
-	$("#ctpSidePanel").addClass("ctpSidePanelWide");
-	$("#ctpSidePanelBackground").addClass("ctpSidePanelWide");
-	$("#ctpIcons").hide('slow');
-	$("#narrower").show('fast');
-}
-
-function sidePanelNarrow() {
-	$("#ctpSidePanel").removeClass("ctpSidePanelWide");
-	$("#ctpSidePanelBackground").removeClass("ctpSidePanelWide");
-	$("#ctpSidePanel").addClass("ctpSidePanelNarrow");
-	$("#ctpSidePanelBackground").addClass("ctpSidePanelNarrow");
-	$("#narrower").hide('fast');
-	$("#ctpIcons").show('slow');
 }
