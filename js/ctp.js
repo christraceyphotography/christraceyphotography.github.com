1b9d3d1685331354877dd2b62bfa75866876ecb2
Chris Tracey <tracey.chris@gmail.com>
2014-05-04 16:25:38 +1000

    WIP external link

--- js/ctp.js
+++ js/ctp.js
@@ -1,9 +1,42 @@
+function getAnchor() {
+  return window.location.hash.replace("#", "");
+}
+
 function ctpInit() {
 	$('.ctpHeader').load('ctpLogo.html');
 	$('.header_icons').load('icons.html');
 	$('.info').load('info.html');
+
+  if (previewMode()) {
+    a = {}
+    a[getAnchor()] = portfolio[getAnchor()];
+    portfolio = a;
+  }
+  $('.gallery').load('gallery.html');
+
 	$('.ctpHeaderUnderMenu').load('ctpLogo.html');
-  $('.mega-hoverlink').click(function(eventObject) { show_500px_photo(eventObject); });
+
+}
+
+function previewMode() {
+  var anchor = getAnchor();
+
+  if ( anchor != "" ) {
+    if ( isNaN(anchor) ) {
+      return false;
+    } else {
+      valid = portfolio[anchor] != undefined ? true : false;
+      return valid;
+    }
+  } else {
+    return false;
+  }
+}
+
+function show_photo(event){
+  photo_id = event.target.id;
+  photo_anchor = "./#" + photo_id;
+  navigate_to_anchor(photo_anchor);
 }
 
 function show_500px_photo(event){
@@ -22,4 +55,9 @@ function handler(event) {
 
 function open_tab(url) {
   window.open(url);
+}
+
+function navigate_to_anchor(anchor){
+  location.href = anchor;
+  location.reload(true);
 }
